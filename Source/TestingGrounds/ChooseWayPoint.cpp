b2efec68bb1a967710d7c087b005b6161d68e2eb
DESKTOP-9S9GUOV\indra <indraneel.max@gmail.com>
2017-06-02 00:25:11 +0530

    [TG-8] Created PatrolRoute as a ActorComponent, this is better way to implement patrolling!

--- Source/TestingGrounds/ChooseWayPoint.cpp
+++ Source/TestingGrounds/ChooseWayPoint.cpp
@@ -4,7 +4,7 @@
 #include "ChooseWayPoint.h"
 #include "BehaviorTree/BlackboardComponent.h"
 #include "AIController.h"  //TODO Remove these couplings
-#include "PatrolGuard.h"
+#include "PatrolRoute.h"
 
 
 //We are going to implement the Behaviour Tree Task in c++ (not use BP)
@@ -15,9 +15,19 @@ EBTNodeResult::Type UChooseWayPoint::ExecuteTask(UBehaviorTreeComponent& OwnerCo
 	//Get Patrol Points
 	auto AIController = OwnerComp.GetAIOwner();
 	auto AIPawn = AIController->GetPawn();
-	auto PatrolGuard = Cast<APatrolGuard>(AIPawn);
-	auto PatrolPoints = PatrolGuard->PatrolPoints_CPP;
-
+	auto PatrolRoute = AIPawn->FindComponentByClass<UPatrolRoute>();
+	if (!ensure(PatrolRoute))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("No Patrol Route component found"));
+		return EBTNodeResult::Failed;
+	}
+	
+	auto PatrolPoints = PatrolRoute->GetPatrolPoints();
+	if (PatrolPoints.Num() == 0)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Patrol Points are not yet Set on the Actor"));
+		return EBTNodeResult::Failed;
+	}
 	//Set Next WayPoint
 	auto BlackBoard = OwnerComp.GetBlackboardComponent();
 	auto index = BlackBoard->GetValueAsInt(IndexKey.SelectedKeyName);
